fd34c9694e34c643524bf3aa974c2c1e20500f9c
Артём Павлов [Artyom Pavlov] <newpavlov@gmail.com>
2021-12-08 04:29:00 +0300

    update #![doc(..)] attributes

--- crypto_kx/src/lib.rs
+++ crypto_kx/src/lib.rs
@@ -48,8 +48,9 @@
 
 #![no_std]
 #![doc(
-    html_logo_url = "https://raw.githubusercontent.com/RustCrypto/meta/master/logo.svg",
-    html_favicon_url = "https://raw.githubusercontent.com/RustCrypto/meta/master/logo.svg"
+    html_logo_url = "https://raw.githubusercontent.com/RustCrypto/media/6ee8e381/logo.svg",
+    html_favicon_url = "https://raw.githubusercontent.com/RustCrypto/media/6ee8e381/logo.svg",
+    html_root_url = "https://docs.rs/crypto_kx/0.0.2"
 )]
 #![warn(missing_docs, rust_2018_idioms)]
 
