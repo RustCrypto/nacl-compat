9b2e63819e2c2ae5566ba45d9e2d9274937d986b
Tony Arcieri <bascule@gmail.com>
2023-05-16 08:41:05 -0600

    crypto_box: make `seal` and `unseal` inherent methods (#118)
    - Moves `seal` to `PublicKey::seal`
    - Moves `seal_open` to `SecretKey::unseal`
    
    This should make these slightly more convenient to use by reducing the
    number of imports required.

--- crypto_box/src/public_key.rs
+++ crypto_box/src/public_key.rs
@@ -2,6 +2,13 @@ use crate::{SecretKey, KEY_SIZE};
 use core::cmp::Ordering;
 use curve25519_dalek::MontgomeryPoint;
 
+#[cfg(feature = "seal")]
+use {
+    crate::{get_seal_nonce, SalsaBox, TAG_SIZE},
+    aead::{rand_core::CryptoRngCore, Aead},
+    alloc::vec::Vec,
+};
+
 #[cfg(feature = "serde")]
 use serdect::serde::{de, ser, Deserialize, Serialize};
 
@@ -31,6 +38,30 @@ impl PublicKey {
     /// Serialize this public key as bytes.
     pub fn to_bytes(&self) -> [u8; KEY_SIZE] {
         self.0.to_bytes()
+    }
+
+    /// Implementation of `crypto_box_seal` function from [libsodium "sealed boxes"].
+    ///
+    /// Sealed boxes are designed to anonymously send messages to a recipient given their public key.
+    ///
+    /// [libsodium "sealed boxes"]: https://doc.libsodium.org/public-key_cryptography/sealed_boxes
+    #[cfg(feature = "seal")]
+    pub fn seal(
+        &self,
+        csprng: &mut impl CryptoRngCore,
+        plaintext: &[u8],
+    ) -> Result<Vec<u8>, aead::Error> {
+        let mut out = Vec::with_capacity(KEY_SIZE + TAG_SIZE + plaintext.len());
+        let ephemeral_sk = SecretKey::generate(csprng);
+        let ephemeral_pk = ephemeral_sk.public_key();
+        out.extend_from_slice(ephemeral_pk.as_bytes());
+
+        let nonce = get_seal_nonce(&ephemeral_pk, self);
+        let salsabox = SalsaBox::new(self, &ephemeral_sk);
+        let encrypted = salsabox.encrypt(&nonce, plaintext)?;
+        out.extend_from_slice(&encrypted);
+
+        Ok(out)
     }
 }
 
